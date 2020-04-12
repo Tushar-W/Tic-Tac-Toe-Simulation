d25a20cf3cf0865bc9d59b733c3eb45843118212
Tushar-W <twankhede95@gmail.com>
2020-04-12 23:19:47 +0530

    ADDED:function for check winning or tie game

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -4,38 +4,20 @@ echo "Welcome To Tic-Tac-Toe Simulation"
 #declaration of array playBoard
 declare -a playBoard
 #CONSTANTS
-NOUGHT="X"
-CROSS="O"
+NOUGHT="O"
+CROSS="X"
+TOTAL_CELLS=10
 #VARIABLES
+checkWin=0
 player=""
+computer=""
 
-#resetting board
-function resetBoard() {
-	for((count=1; count<10; count++))
-	do
-		playBoard[$count]=" "
-	done
-}
 
-#Assigning nought or cross to player
-function assignNoughtOrCross() {
-	letter=$((RANDOM%2))
-	if [ $letter -eq 0 ];
+#checking is game tie
+function isGameTie() {
+	if [ $i -eq $TOTAL_CELLS ];
 	then
-		player=$NOUGHT
-	else
-		player=$CROSS
-	fi
-}
-
-#checking who play first in game
-function checkWhoPlayFirst() {
-	turn=$((RANDOM%2))
-	if [ $turn -eq 0 ];
-	then
-		echo "Player Play First"
-	else
-		echo "Computer Play First"
+		echo "Game Is Tie"
 	fi
 }
 
@@ -43,16 +25,154 @@ function checkWhoPlayFirst() {
 function displayBoard() {
 	echo " Play Board "
 	echo "---------"
-	for((count=1; count<10; count++))
+	for((count=1; count<$TOTAL_CELLS; count++))
 	do
-		echo "${playBoard[$count]} | ${playboard[$((count++))]} | ${playBoard[$((count++))]}"
+		echo "${playBoard[$count]} | ${playBoard[$((++count))]} | ${playBoard[$((++count))]}"
 		echo "---------"
 	done
 }
 
-resetBoard
-echo ${playBoard[@]}
-assignNoughtOrCross
-echo "player:"$player
-checkWhoPlayFirst
-displayBoard
+#checking winner at row
+function checkingRow() {
+	for ((count=1; count<$TOTAL_CELLS; count=count+3))
+	do
+		temp=$count
+		if [[ ${playBoard[$temp]} == $letter ]] && [[ ${playBoard[$temp]} == ${playBoard[$((temp+1))]} ]] && [[ ${playBoard[$((temp+1))]} == ${playBoard[$((temp+2))]} ]]
+		then
+			echo "$name Is Win"
+			checkWin=1
+			exit
+		fi
+	done
+}
+
+#checking winner at column
+function checkingColumn() {
+	for ((count=1; count<4; count++))
+	do
+		temp=$count
+		if [[ ${playBoard[$temp]} == $letter ]] && [[ ${playBoard[$temp]} == ${playBoard[$((temp+3))]} ]] && [[ ${playBoard[$((temp+3))]} == ${playBoard[$((temp+6))]} ]]
+		then
+			echo "$name Is Win"
+			checkWin=1
+			exit
+		fi
+	done
+}
+
+#checking winner at diagonal
+function checkingDiagonal() {
+		if [[ ${playBoard[1]} == $letter ]] && [[ ${playBoard[5]} == $letter ]] && [[ ${playBoard[9]} == $letter ]]
+		then
+			echo "$name Is Win"
+			checkWin=1
+		elif [[ ${playBoard[3]} == $letter ]] && [[ ${playBoard[5]} == $letter ]] && [[ ${playBoard[7]} == $letter ]]
+		then
+			echo "$name Is win"
+			checkWin=1
+		fi
+}
+
+#checking winner
+function checkWinner() {
+	letter=$1
+	name=$2
+	checkingRow
+	checkingColumn
+	checkingDiagonal
+}
+
+#check Position Available
+function checkPositionAvailable() {
+		position=$1
+		if [[ ${playBoard[$position]} == " " ]];
+		then
+			playBoard[position]=$2
+		else
+			echo "Please Enter Valid Position"
+			switchPlayer
+		fi
+}
+
+#player turn
+function playerTurn() {
+	read -p "Enter Position:" position
+	checkPositionAvailable $position $player
+	displayBoard
+	checkWinner $player "Player"
+	number=1
+}
+
+#computer turn
+function computerTurn() {
+		comPos=$((RANDOM%9 + 1))
+		echo "Computer Position" $comPos
+		checkPositionAvailable $comPos $computer
+		displayBoard
+		checkWinner $computer "Computer"
+		number=0
+}
+
+#switch players
+function switchPlayer() {
+	if [ $number -eq 0 ];
+	then
+		echo "Player turn"
+		playerTurn
+	else
+		echo "Computer Turn"
+		computerTurn
+	fi
+}
+
+#Assigning nought or cross to player
+function assignNoughtOrCross() {
+	if [ $number -eq 0 ];
+	then
+		player=$CROSS
+		computer=$NOUGHT
+	else
+		player=$NOUGHT
+		computer=$CROSS
+	fi
+}
+
+#checking who play first in game
+function checkWhoPlayFirst() {
+	number=$((RANDOM%2))
+	if [ $number -eq 0 ];
+	then
+		echo "Player Play First"
+		assignNoughtOrCross
+	else
+		echo "Computer Play First"
+		assignNoughtOrCross
+	fi
+}
+
+#resetting board
+function resetBoard() {
+	for((count=1; count<$TOTAL_CELLS; count++))
+	do
+		playBoard[$count]=" "
+	done
+}
+
+function main() {
+	resetBoard
+	checkWhoPlayFirst
+	displayBoard
+	for ((i=1; i<$TOTAL_CELLS; i++))
+	do
+		if [ $checkWin -eq 0 ];
+		then
+			switchPlayer
+			isGameTie
+		else
+			break
+		fi
+	done
+}
+
+#calling main function
+main
